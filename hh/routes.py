888ea0076ed34a57b617e1531b165e89c6103f3b
radiantly <44368997+radiantly@users.noreply.github.com>
2021-01-30 12:48:20 +0530

    Fix submit button

--- hh/routes.py
+++ hh/routes.py
@@ -1,8 +1,19 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request
 from hh import app
 
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if (
+        request.form
+        and "problem-details" in request.form
+        and request.form["problem-details"]
+    ):
+        problemDetails = request.form["problem-details"]
+        print(problemDetails)
     return render_template("home.html")
+
+
+@app.route("/list")
+def problemlist():
+    pass
