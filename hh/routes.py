37a5cccf1a0e00c7a2f0f8e661a1f9e1554b2658
Jason13201 <jason13201@outlook.com>
2021-01-30 15:01:06 +0530

    Added backend DB code

--- hh/routes.py
+++ hh/routes.py
@@ -2,6 +2,18 @@ from flask import render_template, url_for, request
 from hh import app
 
 
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from config import username, password
+
+cloud_config = {"secure_connect_bundle": "secure-connect-breakfastproblems.zip"}
+auth_provider = PlainTextAuthProvider(username, password)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect()
+
+id = 1
+
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     if (
@@ -13,6 +25,15 @@ def home():
         print(problemDetails)
         # String containing the problem submission
         # Store in db
+        global id
+        session.execute(
+            """
+        INSERT INTO breakfast.problems (id, column)
+        VALUES (%s, %s)
+        """,
+            (id, problemDetails),
+        )
+        id += 1
 
         return render_template("submit.html")
 
@@ -22,4 +43,8 @@ def home():
 @app.route("/list")
 def problemlist():
     # Retrieve all records from db
+    listOfProblems = []
+    rows = session.execute("SELECT id, column FROM breakfast.problems;")
+    for row in rows:
+        listOfProblems.append(row.column)
     pass
